be4f2775f0158833f457fcaf319c13c0ff9d0249
Santosh Chauhan <santoshchhn6@gmail.com>
2023-02-17 00:31:58 +0530

    getStaticPaths

--- src/pages/events/[cat]/index.tsx
+++ src/pages/events/[cat]/index.tsx
@@ -1,17 +1,51 @@
+import Image from "next/image";
 import React from "react";
 
-const EventPerCity = () => {
+const EventPerCity = ({ data }: { data: [] }) => {
   return (
     <div>
       <h1>Event in Landon</h1>
-      <a href="/event/event1">event1</a>
-      <a href="/event/event2">event2</a>
-      <a href="/event/event3">event3</a>
-      <a href="/event/event4">event4</a>
-      <a href="/event/event5">event5</a>
-      <a href="/event/event6">event6</a>
+      <div>
+        {data.map(
+          (
+            e: {
+              id: string;
+              city: string;
+              image: string;
+              title: string;
+              description: string;
+            },
+            i
+          ) => (
+            <a key={i} href={`/events/${e.city}/${e.id}`}>
+              <Image src={e.image} alt={e.title} width={200} height={200} />
+              <h2>{e.title}</h2>
+              <p>{e.description}</p>
+            </a>
+          )
+        )}
+      </div>
     </div>
   );
 };
 
 export default EventPerCity;
+
+export async function getStaticPaths() {
+  const { events_categories } = await import("../../../../data/data.json");
+  const allPaths = events_categories.map((e) => ({
+    params: {
+      cat: e.id.toString(),
+    },
+  }));
+  console.log(allPaths);
+  return { paths: allPaths, fallback: false };
+}
+
+export async function getStaticProps(context: any) {
+  const id = context?.params.cat;
+  const { allEvents } = await import("../../../../data/data.json");
+  const data = allEvents.filter((e) => e.city === id);
+  console.log(data);
+  return { props: { data } };
+}
