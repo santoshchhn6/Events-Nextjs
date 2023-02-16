a17b5f1b966fd5d51941d126eca6d5be6fdaecf4
Santosh Chauhan <santoshchhn6@gmail.com>
2023-02-16 21:26:55 +0530

    getStaticProps (prerender static page)

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,25 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
-function index() {
+function Events({ data }: { data: [] }) {
   return (
     <div>
       <h1>Events</h1>
       <div>
-        <a href="">
-          <img src="" alt="" />
-          <h2>Events in Landon</h2>
-        </a>
-        <a href="">
-          <img src="" alt="" />
-          <h2>Events in Barcelona</h2>
-        </a>
-        <a href="">
-          <img src="" alt="" />
-          <h2>Events in San Francisco</h2>
-        </a>
+        {data.map((e: { id: string; title: string; image: string }, i) => (
+          <a key={i} href={`/events/${e.id}`}>
+            <Image src={e.image} alt={e.title} width={200} height={200} />
+            <h2>{e.title}</h2>
+          </a>
+        ))}
       </div>
     </div>
   );
 }
 
-export default index;
+export default Events;
+
+export async function getStaticProps() {
+  const { events_categories } = await import("../../../data/data.json");
+  console.log(events_categories);
+
+  return {
+    props: {
+      data: events_categories,
+    },
+  };
+}
